f400c01ba5a40f3df11c01d080f59548788e5d25
avahak <a.vahakangas.work@gmail.com>
2024-03-06 02:08:23 +0200

    trivial change - date locale 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
     plugins: [react()],
     base: '/test',
     define: {
-        BUILD_DATE: JSON.stringify(new Date().toLocaleDateString('en-FI')),
+        BUILD_DATE: JSON.stringify(new Date().toLocaleString('en-FI', { 
+            timeZone: 'Europe/Helsinki', 
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+        })),
     },
 })
